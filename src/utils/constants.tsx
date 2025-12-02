1fe1ab32cb87b2dcb12b9740de13dec988d276c6
andriawan24 <fawaznaufal23@gmail.com>
2025-12-02 11:45:51 +0700

    fix: change siteconfig url

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  url: "https://bo.lexicon.id",
+  url: "https://procurement.lexicon.id",
   name: "Lexicon Beneficial Ownership",
   description:
     "Lexicon Beneficial Ownership is a dedicated company to transparency and integrity in the corporate world. Our mission is to uncover and display the true beneficial ownership of companies, highlighting those involved in corruption. By providing clear, accessible information, we empower citizens, regulators, and investors to make informed decisions. Join us in our quest for accountability and help us shine a light on the hidden faces behind corporate corruption. Discover the truth with Lexicon Beneficial Ownership.",
