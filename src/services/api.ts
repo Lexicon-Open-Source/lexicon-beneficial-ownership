ab9947582899693aca6031622ca72690001519ef
andriawan24 <fawaznaufal23@gmail.com>
2025-12-02 12:56:00 +0700

    feature: Update default API URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 export const BASE_URL =
   (process.env.NEXT_PUBLIC_BASE_URL as string) ||
-  "https://beneficialowner.lexicon.id/api";
+  "https://procurement.lexicon.id/api";
 export const BASE_URL_CHATBOT =
   (process.env.NEXT_PUBLIC_BASE_URL_CHATBOT as string) ||
   "https://7627-103-121-108-197.ngrok-free.app";
